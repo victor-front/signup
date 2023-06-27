03c3c42e4f610f1ee04f63234221f589469f794d
Victor Aguiar <victoriekjkj@gmail.com>
2023-06-27 18:24:25 -0300

    sistema de foto de perfil (seleciona-la) feito com exito.

--- data/script.js
+++ data/script.js
@@ -1,4 +1,27 @@
-document.addEventListener('keydown', function(event){
+const photo = document.querySelector(".pfp");
+const file = document.querySelector(".file-pfp");
+const i0 = document.querySelector(".i0");
+
+photo.addEventListener('click', ()=>{
+	file.click();
+});
+
+file.addEventListener('change', (event)=>{
+	if(file.files.length <= 0){
+		return;
+	}
+	
+	let reader = new FileReader();
+	reader.onload = () =>{
+		photo.style.backgroundImage = `url("${reader.result}")`;
+	};
+	
+	i0.style.display = "none";
+	
+	reader.readAsDataURL(file.files[0]);
+});
+
+document.addEventListener('keydown', (event)=>{
 	let keyCode = event.code;
 	if(keyCode == "Enter"){
 		filtrar();
