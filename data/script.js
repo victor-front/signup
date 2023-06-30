995c9e8118665225b001ed92ceed1305a57b1c85
Victor Aguiar <victoriekjkj@gmail.com>
2023-06-30 19:38:43 -0300

    dei uma mechida no HTML, fiz o CSS da sessão do perfil já criado e fiz verificações no JavaScript e filtro com comparações ao refletir os dados armazenados no perfil. || Preciso adicionar função de deletar/recriar a conta e dar uns detalhes finais.

--- data/script.js
+++ data/script.js
@@ -1,11 +1,19 @@
 const photo = document.querySelector(".pfp");
 const file = document.querySelector(".file-pfp");
 const i0 = document.querySelector(".i0");
+const underfinedPFP = 'url("data/underfined.png")';
+const underfinedDesc = "Nada a dizer."
+let controle = false;
 
 if(localStorage.getItem('lnick')){
+	document.querySelector('.unome').innerHTML = localStorage.getItem('lnick');
+	document.querySelector('.udesc').innerHTML = verificarDescricao(localStorage.getItem('ldesc'));
+	document.querySelector('.photo').style.backgroundImage = verificarFoto(localStorage.getItem('lpfp'));
 	document.querySelector('.user').style.display = "flex";
+	controle = false;
 }else{
 	document.querySelector('.sign-up').style.display = 'flex';
+	controle = true;
 }
 
 photo.addEventListener('click', ()=>{
@@ -30,7 +38,11 @@ file.addEventListener('change', (event)=>{
 document.addEventListener('keydown', (event)=>{
 	let keyCode = event.code;
 	if(keyCode == "Enter"){
-		filtrar();
+		if(controle){
+			filtrar();
+		}else{
+			resetar();
+		}
 	}
 });
 
@@ -53,4 +65,27 @@ function embalar(nick, desc, pfp){
 	if(desc){localStorage.setItem('ldesc', desc);}
 	if(pfp){localStorage.setItem('lpfp', pfp);}
 	location.reload();
+}
+
+function resetar(){
+	localStorage.removeItem('lnick');
+	localStorage.removeItem('ldesc');
+	localStorage.removeItem('lpfp');
+	location.reload();
+}
+
+function verificarFoto(valor){
+	if(valor){
+		return valor;
+	}else{
+		return underfinedPFP;
+	}
+}
+
+function verificarDescricao(valor){
+	if(valor){
+		return valor;
+	}else{
+		return underfinedDesc;
+	}
 }
